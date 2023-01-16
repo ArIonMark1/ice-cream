5de4de1cd9961481d0b5371e2097639b7750aa80
CharlieChaplin13 <BlanshDyubua13@gmail.com>
2023-01-16 21:34:44 +0200

    add reviews-swiper

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,1 +1,20 @@
-new Swiper('.swiper-container');
+// const { Scrollbar } = require("swiper");
+
+// new Swiper('.reviews__slider', {
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     }
+
+//     // slideToClickedSlide: true,
+// });
+
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    slideToClickedSlide: true,
+  });
